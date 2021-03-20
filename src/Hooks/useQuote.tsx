8112ae9d4a7ddf7fb7cbdfef3d19b64a6eb67ad7
Dean Harris <dean@harris.tc>
2021-03-20 15:53:13 +1000

    refactor: simplify useQuote hook

--- src/Hooks/useQuote.tsx
+++ src/Hooks/useQuote.tsx
@@ -1,23 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-export const useQuote = (arr: any[]) => {
-  const [ind, setInd] = useState<number | null>(null);
-  const randomIndex = (len: number): number =>
-    Math.ceil(Math.random() * len) - 1;
+const randomIndex = (len: number): number => Math.ceil(Math.random() * len) - 1;
+
+export const useQuote = (arr: string[]) => {
+  const [ind, setInd] = useState<number | null>(randomIndex(arr.length));
 
   useEffect(() => {
-
-    setInd(randomIndex(arr.length));
-    window.addEventListener("keydown", (e) => {
-      if (e.keyCode === 32) {
-        setInd(randomIndex(arr.length))
+    const updateIndex: EventListener = (e) => {
+      if (e instanceof KeyboardEvent && e.keyCode === 32) {
+        setInd(randomIndex(arr.length));
       }
-    });
-    return () => window.removeEventListener("keydown", (e) => {
-      if (e.keyCode === 32) {
-        setInd(randomIndex(arr.length))
-      }
-    });
+    };
+    document.addEventListener('keydown', updateIndex);
+    return () => document.removeEventListener('keydown', updateIndex);
   }, [arr.length]);
 
   return ind ? arr[ind] : ind;
